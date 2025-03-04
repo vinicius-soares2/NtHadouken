bc51455856409aa68b7f9d59e05aa52c0e1027be
_int2Eh <99436926+lnt2eh@users.noreply.github.com>
2025-03-04 14:01:13 -0300

    WinDbgDarkMode.bat
    Atualização de script.

--- Tools/WinDbgDarkMode.bat
+++ Tools/WinDbgDarkMode.bat
@@ -11,13 +11,16 @@ if %errorlevel% NEQ 0 (
 color 0a
 chcp 65001
 cls
-:: Exibindo a arte ASCII
+echo.
+echo.
 echo d8b   db d888888b db   db  .d8b.  d8888b.  .d88b.  db    db db   dD d88888b d8b   db 
 echo 888o  88 `~~88~~' 88   88 d8' `8b 88  `8D .8P  Y8. 88    88 88 ,8P' 88'     888o  88 
 echo 88V8o 88    88    88ooo88 88ooo88 88   88 88    88 88    88 88,8P   88ooooo 88V8o 88 
 echo 88 V8o88    88    88~~~88 88~~~88 88   88 88    88 88    88 88`8b   88~~~~~ 88 V8o88 
 echo 88  V888    88    88   88 88   88 88  .8D `8b  d8' 88b  d88 88 `88. 88.     88  V888 
 echo VP   V8P    YP    YP   YP YP   YP Y8888D'  `Y88P'  ~Y8888P' YP   YD Y88888P VP   V8P 
+echo.
+echo.
 
 echo Baixando o arquivo do GitHub...
 curl -L -o "%USERPROFILE%\Downloads\master.zip" https://github.com/lololosys/windbg-theme/archive/refs/heads/master.zip
